1f109db7aebd31ea25c843faf02ab1dfb7d71a65
Andrew Bail <andrew.bail@myob.com>
2020-06-15 17:11:51 +1000

    create base design system

---
+++ packages/app/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "ui-library";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to t-online</h1>
+      <p>
+        <Button>I'm a button</Button>
+      </p>
+    </div>
+  );
+}
