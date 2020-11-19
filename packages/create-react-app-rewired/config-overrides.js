9835befdd353c58b1115620dc1cf90be828c3fe6
Andrew Bail <andrew.bail@myob.com>
2020-11-20 08:31:55 +1100

    remove packages

--- packages/create-react-app-rewired/config-overrides.js
+++ packages/create-react-app-rewired/config-overrides.js
@@ -1,23 +1,6 @@
 const TreatPlugin = require("treat/webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// const { override } = require("customize-cra");
-// const path = require("path");
-
-const addWebpackPlugin = (plugin) => (config) => {
-  config.plugins = plugin.concat(config.plugins);
-  console.log(config);
-  return config;
-};
-
-// module.exports = override(
-//   addWebpackPlugin([
-//     new TreatPlugin({
-//       // outputLoaders: [MiniCssExtractPlugin.loader],
-//     }),
-//     // new MiniCssExtractPlugin(),
-//   ])
-// );
 
 module.exports = function (config, env) {
   config.plugins = [
